ea1dd29605444f27b66a1da32af4442d2fe23f49
Naveen Kumar Matturthi <naveenkumar3273@gmail.com>
2015-07-12 18:17:28 +0530

    Added backbone app structure

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Whataoffer, $*/
+
+
+window.Whataoffer = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Whataoffer.init();
+});
